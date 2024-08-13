459eff6840b947ef24917a3e067cfd77d30e5e98
aalesann <alejandrosanabria74@gmail.com>
2024-08-13 16:45:10 -0300

    Servidor express basado en clases con ruta para crear y obtener productos

---
+++ src/db/connection.js
@@ -0,0 +1,13 @@
+import { connect } from "mongoose";
+
+import { URI } from "../config/conf.js";
+
+export const dbConnection = async () => {
+    try {
+        await connect(URI);
+        console.log('Base de datos conectada')
+    } catch (err) {
+        console.log('Error al conectar a BD: ', err);
+        process.exit();
+    }
+}
